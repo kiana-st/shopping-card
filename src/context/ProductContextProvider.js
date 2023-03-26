1b0ba8b8d689e19c1016507f42cd9ce72758fc40
k.soltani <k.soltani@shadzi.club>
2023-03-26 14:51:19 +0330

    context-api

---
+++ src/context/ProductContextProvider.js
@@ -0,0 +1,24 @@
+import React, { createContext, useEffect, useState } from "react";
+
+//API
+import { getProducts } from "../services/api";
+
+const ProductsContext = createContext();
+
+const ProductContextProvider = (props) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      setProducts(await getProducts);
+    };
+    fetchApi();
+  }, []);
+  return (
+    <ProductsContext.Provider value={products}>
+      {props.children}
+    </ProductsContext.Provider>
+  );
+};
+
+export default ProductContextProvider;
